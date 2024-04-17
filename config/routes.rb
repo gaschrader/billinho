d0a8f00077819331a14ae05154477a3b4551520e
Gabriel Schrader <gaschrader02@gmail.com>
2024-04-17 10:42:06 -0300

    add modules to root route

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
       # root "posts#index"
     end
   end
-  root "institutions#index"
+  root "api/v1/institutions#index"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
