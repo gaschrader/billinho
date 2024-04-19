4124ff4c6920ca8151dba24aed942bb888636092
Gabriel Schrader <gaschrader02@gmail.com>
2024-04-19 15:51:16 -0300

    change locales in application

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module Billinho
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.available_locales = ['en', 'pt-BR']
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
