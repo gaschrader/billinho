dd513d1388e1e4f77de4f056bde8c4d760c40f6c
Gabriel Schrader <gaschrader02@gmail.com>
2024-04-19 15:52:11 -0300

    add second iteration of enrollment and bill creation

--- app/controllers/api/v1/enrollments_controller.rb
+++ app/controllers/api/v1/enrollments_controller.rb
@@ -2,6 +2,7 @@ module Api
   module V1
     class EnrollmentsController < ApplicationController
       before_action :set_enrollment, only: %i[ show update destroy ]
+      after_action :create_bills, only: %i[ create ]
 
       # GET /enrollments
       def index
@@ -18,54 +19,8 @@ module Api
       # POST /enrollments
       def create
         @enrollment = Enrollment.new(enrollment_params)
-        # NAO ESQUECER DE CRIAR FUNC A PARTE E USAR AFTER_ACTION
+
         if @enrollment.save
-          @bills = []
-
-          date = Date.parse(Time.now.strftime("%d/%m/%Y"))
-          begin_next_month = (@enrollment.bill_due_date <= date.day) ? 1 : 0
-
-          if begin_next_month == 1
-            # Esse condicional só cai em um cenário completamente anormal
-            # em que a bill_due_date é 31 e a matrícula é criada dia 31.
-            if not Date.valid_date?(date.year, date.mon + 1, @enrollment.bill_due_date)
-              bill_date = date.at_beginning_of_month.next_month.next_month
-            else
-              bill_date = Date.parse("#{@enrollment.bill_due_date.to_s}/#{date.mon + 1}/#{date.year}")
-            end
-          else
-            if not Date.valid_date?(date.year, date.mon, @enrollment.bill_due_date)
-              bill_date = date.at_beginning_of_month.next_month
-            else
-              bill_date = date.at_beginning_of_month.next_month
-              bill_date = Date.parse("#{@enrollment.bill_due_date.to_s}/#{date.mon}/#{date.year}")
-            end
-          end
-
-          @enrollment.number_of_bills.times do
-            current_bill = Bill.new(
-              bill_cost: @enrollment.total_cost / @enrollment.number_of_bills,
-              due_date: bill_date,
-              enrollment: @enrollment,
-              status: "Aberta"
-            )
-            @bills.append(current_bill.save!)
-
-            if bill_date.day < @enrollment.bill_due_date
-              next_date = bill_date + (@enrollment.bill_due_date - bill_date.day).days
-            else
-              next_date = bill_date + 1.months
-            end
-
-            bill_day, bill_month, bill_year = @enrollment.bill_due_date, next_date.month, next_date.year
-
-            if not Date.valid_date?(bill_year, bill_month, bill_day)
-              bill_date = next_date.at_beginning_of_month.next_month
-            else
-              bill_date = Date.parse("#{bill_day.to_s}/#{bill_month}/#{bill_year}")
-            end
-          end
-
           render json: @enrollment, status: :created, location: url_for([:api, :v1, @enrollment])
         else
           render json: @enrollment.errors, status: :unprocessable_entity
@@ -86,8 +41,52 @@ module Api
         @enrollment.destroy!
       end
 
-      def verify_date date
+      def create_bills
+        @bills = []
 
+        date = Date.parse(Time.now.strftime("%d/%m/%Y"))
+        begin_next_month = (@enrollment.bill_due_date <= date.day) ? 1 : 0
+
+        # Conficional para pegar data da primeira fatura gerada
+        if begin_next_month
+          # Esse condicional só cai em um cenário completamente anormal
+          # em que a bill_due_date é 31 e a matrícula é criada dia 31.
+          if not Date.valid_date?(date.year, date.mon + 1, @enrollment.bill_due_date)
+            bill_date = date.next_month.end_of_month
+          else
+            bill_date = Date.parse("#{@enrollment.bill_due_date.to_s}/#{date.mon + 1}/#{date.year}")
+          end
+        else
+          if not Date.valid_date?(date.year, date.mon, @enrollment.bill_due_date)
+            bill_date = date.end_of_month
+          else
+            bill_date = Date.parse("#{@enrollment.bill_due_date.to_s}/#{date.mon}/#{date.year}")
+          end
+        end
+
+        @enrollment.number_of_bills.times do
+          current_bill = Bill.new(
+            bill_cost: @enrollment.total_cost / @enrollment.number_of_bills,
+            due_date: bill_date,
+            enrollment: @enrollment,
+            status: "Aberta"
+          )
+          @bills.append(current_bill.save!)
+
+          if bill_date.day < @enrollment.bill_due_date
+            bill_date += (@enrollment.bill_due_date - bill_date.day).days
+          else
+            bill_date += 1.months
+          end
+
+          bill_day, bill_month, bill_year = @enrollment.bill_due_date, bill_date.month, bill_date.year
+
+          if not Date.valid_date?(bill_year, bill_month, bill_day)
+            bill_date = bill_date.end_of_month
+          else
+            bill_date = Date.parse("#{bill_day.to_s}/#{bill_month}/#{bill_year}")
+          end
+        end
       end
 
       private
